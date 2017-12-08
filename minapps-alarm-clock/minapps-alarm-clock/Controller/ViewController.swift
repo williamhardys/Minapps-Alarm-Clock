3d6276fbc7756fe13b16d9c3c5fe0cecd3fafb5b
Jibran Ibrahim Syed <jisyed14@gmail.com>
2017-12-08 08:38:08 -0600

    Tweaking Xcode project settings

--- minapps-alarm-clock/minapps-alarm-clock/Controller/ViewController.swift
+++ minapps-alarm-clock/minapps-alarm-clock/Controller/ViewController.swift
@@ -8,16 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController 
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad() 
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning() 
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
