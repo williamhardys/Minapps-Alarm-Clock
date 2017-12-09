55fbe6c463240ad2b4bfd1a50e2ec27f2e774bf8
Jibran Ibrahim Syed <jisyed14@gmail.com>
2017-12-08 21:08:08 -0600

    Clock is displaying the time correctly, but it's in military time format right now

--- minapps-alarm-clock/minapps-alarm-clock/Controller/ClockViewController.swift
+++ minapps-alarm-clock/minapps-alarm-clock/Controller/ClockViewController.swift
@@ -10,21 +10,77 @@ import UIKit
 
 class ClockViewController: UIViewController 
 {
-
+    @IBOutlet weak var lblHoursAndMinutes: UILabel!
+    @IBOutlet weak var lblSeconds: UILabel!
+    
+    
+    private var clockTimer: Timer?
+    
+    
+    
     override func viewDidLoad() 
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        self.updateClockFace()
+        
+        // Just in case
+        self.clockTimer?.invalidate()
+        
+        // Initialize
+        self.initializeClockTimer()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) 
+    {
+        super.viewWillAppear(animated)
+        
+        if self.clockTimer?.isValid == false
+        {
+            self.initializeClockTimer()
+        }
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) 
+    {
+        self.clockTimer?.invalidate()
+        
+        super.viewWillDisappear(animated)
+    }
+    
+    
     override func didReceiveMemoryWarning() 
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         
+        self.clockTimer?.invalidate()
     }
-
-
+    
+    
+    private func initializeClockTimer()
+    {
+        self.clockTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (timer) in
+            self.updateClockFace()
+        })
+    }
+    
+    
+    private func updateClockFace()
+    {
+        let currentTime = Date()
+        let cal = Calendar.current
+        
+        let hours = cal.component(.hour, from: currentTime)
+        let minutes = cal.component(.minute, from: currentTime)
+        let seconds = cal.component(.second, from: currentTime)
+        
+        
+        self.lblHoursAndMinutes.text = "\(hours):\(String(format: "%02d", minutes))"
+        self.lblSeconds.text = ":\(String(format: "%02d", seconds))"
+    }
+    
 }
 
